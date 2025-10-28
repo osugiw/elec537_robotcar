fdb2dbdd0e7cd9a88017957139bd5b5fb51a48a8
mehul-tech <goel.mehul19@gmail.com>
2025-10-28 11:51:40 -0500

    serial port added

--- Arduino Code/DCMotorDriverTest/DCMotorDriverTest.ino
+++ Arduino Code/DCMotorDriverTest/DCMotorDriverTest.ino
@@ -30,29 +30,59 @@ uint8_t motorSpeed = 100;
 
 void setup()
 {
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  motor1.run(motorSpeed); /* value: between -255 and 255. */
-  motor2.run(motorSpeed); /* value: between -255 and 255. */
-  motor3.run(motorSpeed);
-  motor4.run(motorSpeed);
-  delay(2000);
-  motor1.stop();
-  motor2.stop();
-  motor3.stop();
-  motor4.stop();
-  delay(100);
-  motor1.run(-motorSpeed);
-  motor2.run(-motorSpeed);
-  motor3.run(-motorSpeed);
-  motor4.run(-motorSpeed);
-  delay(2000);
-  motor1.stop();
-  motor2.stop();
-  motor3.stop();
-  motor4.stop();
-  delay(2000);
+  if (Serial.available() > 0) { 
+    // Read the incoming byte (as an integer ASCII value)
+    int incomingByte = Serial.read(); 
+
+    // Print what was read for confirmation
+    // Serial.print("I received: ");
+    // Serial.println(incomingByte); 
+    if(incomingByte=='l'){ // take left
+      motor1.run(motorSpeed); /* value: between -255 and 255. */
+      motor2.run(motorSpeed); /* value: between -255 and 255. */
+      motor3.run(motorSpeed);
+      motor4.run(motorSpeed);
+      delay(2000);
+    }
+      
+    if(incomingByte=='r'){  // take right
+      motor1.run(-motorSpeed);
+      motor2.run(-motorSpeed);
+      motor3.run(-motorSpeed);
+      motor4.run(-motorSpeed);
+      delay(2000);
+    }
+
+    
+    if(incomingByte=='f'){ // take front
+      motor1.run(-motorSpeed); /* value: between -255 and 255. */
+      motor2.run(motorSpeed); /* value: between -255 and 255. */
+      motor3.run(-motorSpeed);
+      motor4.run(motorSpeed);
+      delay(2000);
+    }
+
+
+    if(incomingByte=='b'){ // take back
+      motor1.run(motorSpeed); /* value: between -255 and 255. */
+      motor2.run(-motorSpeed); /* value: between -255 and 255. */
+      motor3.run(motorSpeed);
+      motor4.run(-motorSpeed);
+      delay(2000);
+    }
+
+    else{ // stop
+      motor1.stop();
+      motor2.stop();
+      motor3.stop();
+      motor4.stop();
+      delay(100);
+    }
+  }
 }
 
